ea3fc1ce3e4508b1df29b9a5a6de0f4a71e3abd9
DeCastroo <matheuslc36.mc@gmail.com>
2023-07-04 17:46:06 -0300

    First refactor

--- ultrasonicSensor/ultrasonicSensor.ino
+++ ultrasonicSensor/ultrasonicSensor.ino
@@ -13,6 +13,8 @@ const int NUMBER_OF_LEDS = 5;
 const int LEDS_PINS[NUMBER_OF_LEDS] = { 5, 6, 9, 10, 11 };
 const int TRIGGER_PIN = 7;
 const int ECHO_PIN = 8;
+const int MIN_DISTANCE_CM = 2;   // distância mínima em cm
+const int MAX_DISTANCE_CM = 31;  // distância máxima em cm
 
 // Variáveis de estado
 int ledState = LOW;
@@ -33,10 +35,10 @@ unsigned int measureDistance(int triggerPin, int echoPin) {
   sendTriggerPulse(triggerPin);
 
   // Velocidade do som em centímetros por microsegundos
-  const int SPEED_OF_SOUND_CM_US = 0.034;
+  const float SPEED_OF_SOUND_CM_US = 0.034;
 
   unsigned int pulseDuration = pulseIn(echoPin, HIGH);
-  unsigned int distance = (pulseDuration / 2) * SPEED_OF_SOUND_CM_US;
+  unsigned int distance = (pulseDuration / 2.0) * SPEED_OF_SOUND_CM_US;
 
   return distance;
 }
@@ -46,26 +48,44 @@ void binaryDistance(unsigned int distance) {
   int divisor;
   int remainder;
 
-  for (int i = 0; i < NUMBER_OF_LEDS; i++) {  // for que atualiza todos os leds neste loop
-    /* Aqui estamos fazendo i+1 left-shifts no número
-      1, isto é, estamos elevando 2 na potência i+1. */
-    divisor = 1 << (i + 1);
-    remainder = distance % divisor;        //calcula o resto do numero do loop que estamos pelo divisor atual(em qual led estamos)
-    ledState = remainder / (1 << i);       //divide o resto pelo valor atribuido ao led. (0=LOW e 1=HIGH)
-    digitalWrite(LEDS_PINS[i], ledState);  //atualiza o led "i" com seu estado "ledState"
-  }
-  delay(500);
-  powerOffLeds();
-}
-
-
-void powerOffLeds() {
+  // Este loop atualiza o valor de cada led para cada distância medida
   for (int i = 0; i < NUMBER_OF_LEDS; i++) {
-    digitalWrite(LEDS_PINS[i], LOW);
+    // Aqui estamos fazendo i+1 left-shifts no número
+    // 1, isto é, estamos elevando 2 na potência i+1.
+    divisor = 1 << (i + 1);
+
+    // calcula o resto do numero do loop que estamos pelo divisor atual(em qual led estamos)
+    remainder = distance % divisor;
+
+    // divide o resto pelo valor atribuido ao led. (0=LOW e 1=HIGH)
+    ledState = remainder / (1 << i);
+  
+    digitalWrite(LEDS_PINS[i], ledState);
   }
-  delay(500);
+
+  delay(100);
 }
 
+
+void ledPulse() {
+  for (int i = 0; i < NUMBER_OF_LEDS; i++) {
+    for (int fadeValue = 0; fadeValue <= 225; fadeValue += 5) {
+      analogWrite(LEDS_PINS[i], fadeValue);
+      delay(10);
+    }
+  }
+
+  delay(50);
+
+  for (int i = NUMBER_OF_LEDS - 1; i >= 0; i--) {
+    for (int fadeValue = 255; fadeValue >= 0; fadeValue -= 5) {
+      analogWrite(LEDS_PINS[i], fadeValue);
+      delay(10);
+    }
+  }
+
+  delay(50);
+}
 
 void setup() {
   Serial.begin(9600);
@@ -80,8 +100,14 @@ void setup() {
 
 
 void loop() {
-    distance = measureDistance(TRIGGER_PIN, ECHO_PIN);
-    Serial.println(distance);
+
+  distance = measureDistance(TRIGGER_PIN, ECHO_PIN);
+
+  if (distance >= MIN_DISTANCE_CM && distance <= MAX_DISTANCE_CM) {
     binaryDistance(distance);
-    delay(10);
+  } else {
+    ledPulse();
+  }
+
+  delay(10);
 }
